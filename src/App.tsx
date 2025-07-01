f506e2400da172b61520a08f5d35fafdfacbc760
Alex Lee <alexlee8937@gmail.com>
2025-06-30 18:31:00 -0700

    task: integrated tailwindcss into vite + react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1 className="text-amber-500">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   )
 }

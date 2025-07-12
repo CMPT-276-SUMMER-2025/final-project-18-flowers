f41c0c874f018a35293ad20477db35537e6f23b2
Alex Lee <alexlee8937@gmail.com>
2025-07-12 11:26:08 -0700

    feat: added nav bar, improved file structure, and added Google Maps API

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Regions from "../components/Regions"
+
+const Home = () => {
+  return (
+    <>
+      <div id="bg-image" className="justify-center md:justify-normal">
+        <h1 id="home-title" className="text-amber-50 font-bold text-5xl static left-0 md:absolute md:left-30">Taiwan Explorers</h1>
+      </div>
+      <Regions></Regions>
+    </>
+  )
+}
+
+export default Home

7c2d263fb12ad502b6725dfe51a82f4cc337408f
Alex Lee <alexlee8937@gmail.com>
2025-07-12 16:07:12 -0700

    feat: added static map with basic props and markers

--- src/components/Regions.tsx
+++ src/components/Regions.tsx
@@ -1,19 +1,44 @@
-import { APIProvider, Map, type MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, type MapCameraChangedEvent, ColorScheme, Marker } from '@vis.gl/react-google-maps';
 
 const Regions = () => {
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   return (
-    <div className='flex flex-col w-full justify-center items-center'>
-      <h1>Map of Taipei</h1>
-      <div className='flex'>
+    <div id="regions-container" className='flex flex-col w-full justify-center items-center'>
+      <h1 id="regions-title">Regions of Taipei</h1>
+      <div className='border-0 rounded-4xl overflow-hidden m-6'>
         <APIProvider apiKey={apiKey}>
           <Map 
-            defaultZoom={13} 
-            center={{ lat: 25.0330, lng: 121.5654 }}
-            style={{ width: "500px", height: "500px"}}
+            defaultZoom={8} 
+            defaultCenter={{ lat: 23.6978, lng: 120.9605 }}
+            style={{ width: "600px", height: "720px"}}
+            colorScheme={ColorScheme.LIGHT}
             onCameraChanged={ (ev: MapCameraChangedEvent) =>
               console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-            }>  
+            }
+            // @ts-expect-error ensures expected errors
+            options={{
+              fullscreenControl: false,
+              streetViewControl: false,
+              mapTypeControl: false,
+              disableDefaultUI: true,
+              keyboardShorcuts: false,
+              gestureHandling: 'none'
+            }}
+          >
+            {/* taipei city marker */}
+            <Marker position={{ lat: 25.0329, lng: 121.5654 }}></Marker>  
+            {/* hualien city marker */}
+            <Marker position={{ lat: 23.9742, lng: 121.6016 }}></Marker>  
+            {/* yilan city marker */}
+            <Marker position={{ lat: 24.7571, lng: 121.7539 }}></Marker>
+            {/* taichung city marker */}
+            <Marker position={{ lat: 25.0329, lng: 121.5654 }}></Marker>  
+            {/* taipei city marker */}
+            <Marker position={{ lat: 24.1477, lng: 120.6736 }}></Marker>  
+            {/* taichung city marker */}
+            <Marker position={{ lat: 23.0000, lng: 120.2270 }}></Marker> 
+            {/* kaohsiung city marker */}
+            <Marker position={{ lat: 22.6273, lng: 120.3014 }}></Marker>  
           </Map>
         </APIProvider>
       </div>

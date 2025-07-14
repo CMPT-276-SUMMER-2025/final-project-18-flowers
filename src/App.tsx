966794cf5cd455175fa9c70e1612550ba5cbb438
Alex Lee <alexlee8937@gmail.com>
2025-07-13 22:57:36 -0700

    feat: made destinations page and got cities from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, 
          Routes, 
-         Route 
+         Route,
+         useLocation
 } from 'react-router-dom';
 import { APIProvider } from '@vis.gl/react-google-maps';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Destinations from './pages/Destinations';
 import Interests from './pages/Interests';
+import React from 'react';
 
 function App() {
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
@@ -20,17 +22,42 @@ function App() {
         }}
       >
         <Router>
-          <NavBar></NavBar>
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/destinations' element={<Destinations />}></Route>
-            <Route path='/interests' element={<Interests />}></Route>
-          </Routes>
+          <AppContent />
         </Router> 
       </APIProvider>
     </>
-    
-  )
+  );
+}
+
+function AppContent() {
+  const location = useLocation(); // get current route 
+  const { pathname } = location; // get pathname (e.g., /destinations)
+
+  console.log("pathname: " + pathname);
+
+  if (pathname === "/destinations" || pathname === "/interests") {
+    return (
+      <>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/destinations' element={<Destinations />}></Route>
+          <Route path='/interests' element={<Interests />}></Route>
+        </Routes>
+      </>
+    );
+  }
+
+  return( 
+    <>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/destinations' element={<Destinations />}></Route>
+        <Route path='/interests' element={<Interests />}></Route>
+      </Routes>
+    </> 
+  );
 }
 
 export default App

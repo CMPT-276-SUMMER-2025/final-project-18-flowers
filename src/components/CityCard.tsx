041ee9ddd5ff5ca5567c4d762acd199492ac3778
Alex Lee <alexlee8937@gmail.com>
2025-07-15 13:59:16 -0700

    fix: avoided accessing DOM and relied on JSX in destinations page

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,58 +1,66 @@
-// latitude and longitude of Taiwan
-const taiwanLatLng = { lat: 23.7, lng: 121.0 };
-// 6 cities we're doing for now
-const cities = {
-  // task: turn value into arrays that holds image src and short description of city *DONE*
-  "Taipei City" : ['assets/destinations/taipei.jpg', "Modern capital with night markets and Taipei 101."], 
-  "Hualien City" : ['assets/destinations/hualien.jpg', "Coastal town near cliffs and Taroko Gorge."],
-  "Yilan City" : ['assets/destinations/yilan.jpg', "Peaceful area known for hot springs and farms."],
-  "Taichung City" : ['assets/destinations/taichung.jpg', "Cultural hub and birthplace of bubble tea."],
-  "Tainan City" : ['assets/destinations/tainan.jpg', "Oldest city with temples and local snacks."],
-  "Kaohsiung City" : ['assets/destinations/kaohsiung.jpg', "Port city with art, beaches, and skyline."],
-};
+import { useState, useEffect } from "react"; // use effect needed for API handling, use state to trigger UI updates 
+import { Link } from "react-router-dom"; // use to turn city cards into clickable links 
+import { cities } from "../data/cityData"; // get cities object from cityData.tsx 
 
-for (const key in cities) {
-  console.log(key, cities[key as keyof typeof cities]);
-}
+type Props = { cityname: string }
+const taiwanLatLng = { lat: 23.7, lng: 121.0 }; // latitude and longitude of Taiwan
 
-export async function cityCard(cityname: string) {
-  // const { Place } = await google.maps.importLibrary("places") as google.maps.PlacesLibrary; // access new places API
-  const { PlacesService } = await google.maps.importLibrary("places") as google.maps.PlacesLibrary; // access places service
+export default function CityCard({ cityname } : Props) {
+  const [placeName, setPlaceName] = useState("");
 
-  const dummyMap = document.getElementById('dummy-map') as HTMLDivElement; // create dummy map for PlacesService constructor 
-  if (!dummyMap) return (console.log("ERROR: Dummy map could not be found")) // check for error if map can't be accessed
+  useEffect(() => {
+    async function getCity() {
+      const { PlacesService } = await google.maps.importLibrary("places") as google.maps.PlacesLibrary; // access places service
 
-  const service = new PlacesService(dummyMap as HTMLDivElement); // create new instance of object called service to access methods 
+      const dummyMap = document.getElementById('dummy-map') as HTMLDivElement; // create dummy map for PlacesService constructor 
+      if (!dummyMap) return (console.log("ERROR: Dummy map could not be found")) // check for error if map can't be accessed
 
-  const status = google.maps.places.PlacesServiceStatus;
+      const service = new PlacesService(dummyMap as HTMLDivElement); // create new instance of object called service to access methods 
+      const status = google.maps.places.PlacesServiceStatus; // access status to get access to status constants
 
-  service.textSearch({ language: "en", location: taiwanLatLng, query: cityname }, (results, currStatus) => {
-    // if the response contains a valid result 
-    if (currStatus === status.OK) 
-    {
-      console.log(results);
-      const grid = document.getElementById("destinations-grid")!;
+      const myRequest = { // request to feed textSearch method of service 
+        language: "en", // prioritize english names of places
+        location: taiwanLatLng, // create bias for locations near taiwan 
+        query: cityname, // search by the cityname (e.g. Taipei City, Hualien City, etc.)
+      };
 
-      // extra checking for typescript validity
-      if (results && results.length > 0) 
-      {
-        const card = document.createElement("div")!;
-        const imgSrc = cities[cityname as keyof typeof cities][0];
-        const cityDescription = cities[cityname as keyof typeof cities][1];
-        console.log(imgSrc);
-        card.className = "destination-card";
-        // task: turn into links
-        // task: pretty up the cards and add hover effects 
-        card.innerHTML = `<img src=${imgSrc} class='city-image'/><div class="city-text"><h3>${results[0].name}</h3><p>${cityDescription}</p></div>`
-        grid.append(card);
-      }
-    } 
-    else 
-    {
-      console.log("ERROR: Could not find valid result from textSearch()");
+      service.textSearch(myRequest, (results, currStatus) => {
+        // if the response contains a valid result (status.OK)
+        if (currStatus === status.OK) {
+          if (results && (results.length > 0)) // TypeScript double checking to ensure results is not null
+          setPlaceName(results[0].name!); // set place name using useState to the city name
+          // extra checking for typescript validity
+        } else {
+          console.log("ERROR: Could not find valid result from textSearch()"); // could not find the city 
+        }
+      });
     }
-  });
-}
+    getCity(); // call method 
+  }, [cityname]); // the effect function will only re-run only when the cityname dependency changes values between renders 
+
+  // assign 2 values to 2 variables through "array destructing assignment"
+  // this works because if you look in cityData.tsx, the values are arrays of size 2, ["img value", "description value"]
+  const [imgSrc, description] = cities[cityname as keyof typeof cities] || ["", ""]; // 
+
+  // create a clean url path (Hualien%20City -> hualien-city)
+  const formatted = cityname.toLowerCase().replace(/\s+/g, "-");
+
+  // Note: For this project, it's heavily recommended to not access DOM such as .innerHTML, or methods like createElement("element")
+  // Please use JSX to create and update the UI
+  return( 
+    <div>
+      <Link to={`/destinations/${formatted}`}>
+        <div className="destination-card">
+          <img src={imgSrc} className="city-image" alt={placeName}/>
+          <div className="city-text">
+            <h3>{placeName}</h3>
+            <p>{description}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 // if value on left is (null) or (undefined) use right side value instead (??)
 

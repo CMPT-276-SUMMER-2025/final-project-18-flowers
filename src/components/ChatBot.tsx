5f1137dfbc665fc74723fc2dc47d24261288223e
Tian Liang Zheng <tianliangzheng.tlz@gmail.com>
2025-07-22 18:56:14 -0700

    task: add chatbot component, .chatbot style

---
+++ src/components/ChatBot.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"; 
+import ReactMarkdown from "react-markdown";
+
+export default function ChatBot() {
+  //store user input to send to api 
+  return (
+    <div className="chatbot">
+      <p>
+        What do you want to know?
+      </p>
+    </div>
+  )
+}
+

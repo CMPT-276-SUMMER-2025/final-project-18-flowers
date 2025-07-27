123251ede152c6e33382a08f26f9e743ad2d3350
TLZ-05 <tianliangzheng.tlz@gmail.com>
2025-07-27 15:50:22 -0700

    chore, styling: install remark gfm to properly display tables, add background color to ig reponse

--- src/components/ItineraryGenerator.tsx
+++ src/components/ItineraryGenerator.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm';
 
 export default function ItineraryGenerator() {
   //track user's chosen selected options
@@ -225,7 +226,7 @@ export default function ItineraryGenerator() {
         <div className="ig-response">
           <h3>Itinerary</h3>
           <section>
-            <ReactMarkdown>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
               {response}
             </ReactMarkdown>
           </section>

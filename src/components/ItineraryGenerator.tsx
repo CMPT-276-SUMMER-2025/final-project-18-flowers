79cfaf32c738aaf62861a62147801017971add9f
TLZ-05 <tianliangzheng.tlz@gmail.com>
2025-07-26 17:40:23 -0700

    chore: setup ItineraryGenerator

---
+++ src/components/ItineraryGenerator.tsx
@@ -0,0 +1,9 @@
+export default function ItineraryGenerator() {
+  return (
+    <div>
+      <h1>Itinerary Generator</h1>
+      <p>This component will generate an itinerary based on user preferences.</p>
+      {/* Additional functionality will be added here */}
+    </div>
+  );
+}
--- src/pages/PlanTrip.tsx
+++ src/pages/PlanTrip.tsx
@@ -1,8 +1,10 @@
 import PlanTripMap from "../components/PlanTripMap"
+import ItineraryGenerator from "../components/ItineraryGenerator"
 
 const PlanTrip = () => {
   return (
     <div>
+      <ItineraryGenerator />
       <PlanTripMap></PlanTripMap>
     </div>
   )
